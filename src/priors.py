b197704cf140a021020026313824e901e1c26126
Lukas Schumacher <lukas@meth134.psi.uni-heidelberg.de>
2022-10-28 09:49:11 +0200

    gpddm update

--- src/priors.py
+++ src/priors.py
@@ -19,23 +19,23 @@ def diffusion_prior(batch_size, n_cond=4):
     ndt = rng.gamma(1.5, 1/5.0, batch_size)
     return np.c_[v, a, ndt]
 
-def diffusion_prior_gp(batch_size, n_cond=4):
-    """ TODO
+# def diffusion_prior_gp(batch_size, n_cond=4):
+#     """ TODO
 
-    Parameters
-    ----------
-    TODO
+#     Parameters
+#     ----------
+#     TODO
 
-    Returns
-    -------
-    TODO
-    """
+#     Returns
+#     -------
+#     TODO
+#     """
 
-    rng = np.random.default_rng()
-    v = rng.gamma(2.5, 1/2.0, (batch_size, n_cond))
-    a = rng.normal(1.5, 0.3, batch_size)
-    ndt = rng.normal(0.5, 0.1, batch_size)
-    return np.c_[v, a, ndt]
+#     rng = np.random.default_rng()
+#     v = rng.gamma(2.5, 1/2.0, (batch_size, n_cond))
+#     a = rng.normal(1.5, 0.3, batch_size)
+#     ndt = rng.normal(0.5, 0.1, batch_size)
+#     return np.c_[v, a, ndt]
 
 
 def random_walk_prior(batch_size, n_params, alpha=1., beta=25.):
@@ -52,5 +52,6 @@ def random_walk_prior(batch_size, n_params, alpha=1., beta=25.):
 
     return default_rng().beta(alpha, beta, size=(batch_size, n_params))
 
-def length_scale_prior(batch_size, n_params, lower=0.1, upper=20.):
+
+def length_scale_prior(batch_size, n_params, lower=0.1, upper=10.):
     return np.random.default_rng().uniform(lower, upper, size=(batch_size, n_params)).astype(np.float32)
